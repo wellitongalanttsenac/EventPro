b733db4dfde70bb58c4bd198881085de87acd9cc
welliton.caetano <wellitongalant49@gmail.com>
2026-08-21 20:39:44 -0300

    Create DTO

---
+++ src/main/java/com/example/eventpro/DTOs/LoginRequest.java
@@ -0,0 +1,5 @@
+package com.example.eventpro.DTOs;
+
+// Tipo de entididade que nao recebe valor
+public record LoginRequest(String email, String senha) {
+}
--- src/main/java/com/example/eventpro/controller/UsuarioController.java
+++ src/main/java/com/example/eventpro/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.example.eventpro.controller;
 
 import com.example.eventpro.entities.Usuario;
 import com.example.eventpro.repository.UsuarioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +14,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name = "Usuarios", description = "Grupo de api responsavel a estruturar de criação e consultade usuarios do sistema")
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @GetMapping
+    @Operation(summary = "metodo de consulta de lista de usuario", description = "Metodo responsavel pela consulta de todos os usuarios sem filtros")
     public ResponseEntity<?> listarTodos(){
 
         return ResponseEntity.ok(usuarioRepository.findAll());
@@ -25,6 +29,7 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "metodo para ciração de usuario", description = "Metodo responsavel pela criação de usuarios")
     public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario){
         var usuarioBd = usuarioRepository.save(usuario);
         return ResponseEntity.ok(usuarioBd);
